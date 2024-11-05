ccac597b9f1f6ef05a6dde35f351807eec79c1ea
EmanuelMiltonGonzalesQuiroz <emanuelgonzales8209@gmail.com>
2024-11-05 17:55:28 -0400

    ui update 05 11 2024

--- src/components/UI/CustomSelect.jsx
+++ src/components/UI/CustomSelect.jsx
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Select from 'react-select';
 import { db } from '../../connection/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 const CustomSelect = ({ collectionName, placeholder, onChange, selectedValue }) => {
   const [options, setOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchOptions = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, collectionName));
-        const optionsList = querySnapshot.docs.map((doc) => {
-          const data = doc.data();
-          const label = data.name || data.nombre || data.username || 'Sin nombre';
-          
-          return { 
-            label: label,
-            value: doc.id,
-            ...data,
-          };
-        });
-        setOptions(optionsList);
-      } catch (error) {
-        console.error('Error fetching options: ', error);
-      }
-    };
+  // Función para cargar los datos de la colección
+  const fetchOptions = async () => {
+    setIsLoading(true); // Indicador de carga
+    try {
+      const querySnapshot = await getDocs(collection(db, collectionName));
+      const optionsList = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        const label = data.name || data.nombre || data.username || 'Sin nombre';
 
+        return {
+          label: label,
+          value: doc.id,
+          ...data,
+        };
+      });
+      setOptions(optionsList);
+    } catch (error) {
+      console.error('Error fetching options: ', error);
+    }
+    setIsLoading(false); // Desactivar indicador de carga
+  };
+
+  // Cargar opciones cuando se hace clic en el componente Select
+  const handleMenuOpen = () => {
     fetchOptions();
-  }, [collectionName]);
+  };
 
   return (
     <Select
@@ -36,6 +41,8 @@ const CustomSelect = ({ collectionName, placeholder, onChange, selectedValue }) 
       onChange={onChange}
       placeholder={placeholder}
       className="flex-grow"
+      onMenuOpen={handleMenuOpen} // Llama a fetchOptions al abrir el menú
+      isLoading={isLoading} // Muestra un indicador de carga si está activo
     />
   );
 };
